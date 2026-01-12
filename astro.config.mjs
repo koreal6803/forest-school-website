76220bd4dd7c7a4b6af012ac576349fb2afafc15
Cheng-Yu Han <koreal6803@gmail.com>
2026-01-12 16:54:44 +0100

    Configure for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://koreal6803.github.io',
+  base: '/forest-school-website',
   vite: {
     plugins: [tailwindcss()]
   }
